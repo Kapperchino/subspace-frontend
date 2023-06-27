7d1fbf6c7b041f492293f1934b8e4d0c1f6920fd
Kapperchino <kamuran12580@gmail.com>
2023-06-26 20:59:04 -0400

    fixing stuff to make it run on iphone

---
+++ lib/posts/postcardContent.dart
@@ -0,0 +1,46 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:frontend/models/post.dart';
+import 'package:frontend/posts/post.dart';
+import 'package:frontend/posts/voteWidget.dart';
+import 'package:go_router/go_router.dart';
+
+class PostCardContent extends StatelessWidget {
+  const PostCardContent({super.key, required this.postData});
+
+  final Post postData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Column(
+      children: [
+        Expanded(
+            child: FittedBox(
+                fit: BoxFit.fitWidth,
+                child: Text(
+                  postData.topic,
+                ))),
+        if (postData.type == ContentType.text)
+          const SizedBox(width: 0, height: 0),
+        if (postData.type == ContentType.picture)
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.network(
+              postData.content,
+              width: 120,
+              height: 120,
+            ),
+          ),
+        Expanded(
+            child: FittedBox(
+                fit: BoxFit.fill,
+                child: Text(
+                  postData.body,
+                  overflow: TextOverflow.ellipsis,
+                ))),
+      ],
+    ));
+  }
+}
