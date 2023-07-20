dd4c532431e918e6ccbd8d63af866fe4e4d867ec
Kapperchino <kamuran12580@gmail.com>
2023-07-20 15:01:53 -0400

    fix for pathing

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:frontend/mainapp.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:go_router/go_router.dart';
 
 void main() async {
   await GetStorage.init();
+  GoRouter.optionURLReflectsImperativeAPIs = true;
   runApp(MainApp());
 }
